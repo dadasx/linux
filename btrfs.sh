633b0cc775ba6d169c9b87ddb014b3c90652e643
dadasx <55772771+dadasx@users.noreply.github.com>
2020-09-29 14:22:17 +0800

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -31,10 +31,6 @@ echo "                          "
 
 sudo cat /etc/fstab
 
-echo "-----------------------------------------"
-echo "  complete btrfs subvolume list ...      "
-echo "  B06170026 2020/09/28 final             "
-echo "-----------------------------------------"
 
 echo "                          "
 
